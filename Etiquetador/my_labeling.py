719b07fe9e23deca81b6cfe84ca4c4f33af32ec5
Janpb1 <janplanasbatllori@gmail.com>
2023-05-19 17:50:31 +0200

    Mostrar imagenes juntas terminal

--- Etiquetador/my_labeling.py
+++ Etiquetador/my_labeling.py
@@ -1,3 +1,4 @@
+
 __authors__ = 'TO_BE_FILLED'
 __group__ = 'TO_BE_FILLED'
 
@@ -7,6 +8,7 @@ from PIL import Image
 from Kmeans import *
 from KNN import *
 import time as t
+import matplotlib.pyplot as plt
 
 def Retrieval_by_color(list_img, color_labels, color_question):
     
@@ -20,7 +22,7 @@ def Retrieval_by_color(list_img, color_labels, color_question):
         else:
             for color in color_question:
                 for j in range(len(color_labels[i])):
-                    if color_labels[i][j] == color:
+                    if color_labels[i][j] == color_question:
                         trobats.append(list_img[i])
     
     return trobats
@@ -71,11 +73,26 @@ def Kmean_statistics(Kmeans_list, Kmax):
         iter[i] = Kmeans.num_iter
         
     return WCD, time, iter
+
+
+def mostrar_imagenes(imagenes):
+    num_imagenes = len(imagenes)
+    fig = plt.figure(figsize=(12, 8))
+
+
+    for i, imagen in enumerate(imagenes):
+        ax = fig.add_subplot(1, num_imagenes, i+1)
+        ax.imshow(imagen, cmap='gray')  # Utiliza cmap='gray' si las imágenes son en escala de grises
+        ax.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
             
             
     
 if __name__ == '__main__':
-
+    
     # Load all the images and GT
     train_imgs, train_class_labels, train_color_labels, test_imgs, test_class_labels, \
         test_color_labels = read_dataset(ROOT_FOLDER='./images/', gt_json='./images/gt.json')
@@ -84,22 +101,28 @@ if __name__ == '__main__':
     classes = list(set(list(train_class_labels) + list(test_class_labels)))
     
 
-    test_imgs = test_imgs[:40]
+    #test_imgs = test_imgs[:100]
     knn = KNN(train_imgs, train_class_labels)
     color_results = []
     label_results = knn.predict(test_imgs, 10)
     for image in test_imgs:
-        km = KMeans(image, 3)
+        km = KMeans(image, 7)
         km.fit()    
         colors = get_colors(np.array([list(km.centroids[0]), list(km.centroids[1]), list(km.centroids[2])]))
         color_results.append(colors)
 
-    print("Retrieving grey flip-flops")
-    grey_flip_flops = Retrieval_combined(test_imgs, label_results, color_results, "Flip Flo", "Grey")
-    for image in grey_flip_flops:
-        imageObj = Image.fromarray(image)
-        imageObj.show()
-
+    print("Mostrando pantalones negros")
+    pantalones_negros = Retrieval_combined(test_imgs, label_results, color_results, "Jeans", "Black")
+    mostrar_imagenes(pantalones_negros)
+    
+    print("Mostrando ropa verde")
+    ropa_verde = Retrieval_by_color(test_imgs[:100], color_results,"Green")
+    mostrar_imagenes(ropa_verde)
+    
+    print("Mostrando vestidos")
+    vestidos = Retrieval_by_shape(test_imgs[:100], label_results,"Dresses")
+    mostrar_imagenes(vestidos)
+    
     # You can start coding your functions here
 
 
